d80f1f0126554684859f8775059fee7d6104cda2
ahmedbda <prsmul@pm.me>
2025-11-30 18:45:40 +0100

    user and computer name getters

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,15 +1,21 @@
 #include <windows.h>
-#include <iostream>
 
 // using C naming rules so this C++ code can by called by the python main without errors (name mangling)
 extern "C" {    
-    //__declspec(dllexport) to export our library in a dll format when compiling 
-    __declspec(dllexport) bool IsDebuggerDetected() {
-        return IsDebuggerPresent(); 
+    // __declspec(dllexport) to export our library in a dll format when compiling 
+    __declspec(dllexport) bool IsDebuggerDetected(){
+        return IsDebuggerPresent(); // from the windows header
     }
 
-    //simple testing function to check if the .dll is called properly
-    __declspec(dllexport) int Add(int a, int b) {
-        return a + b;
+    __declspec(dllexport) void GetSystemUserName(char* buffer, int size) {
+    // typecasting the int (python) to a DWORD (the unsigned int of windows, so we get no errors when calling the function) 
+    DWORD bufferSize = size;
+    GetUserNameA(buffer, &bufferSize); // from the windows header 
+    }
+
+    // works the same as GetUserNameA
+    __declspec(dllexport) void GetSystemComputerName(char* buffer, int size) {
+    DWORD bufferSize = size;
+    GetComputerNameA(buffer, &bufferSize);  
     }
 }
