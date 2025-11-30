97f7cfe4af41b62091b69cb6266f6a4e2ec6dd99
ahmedbda <prsmul@pm.me>
2025-11-30 20:09:15 +0100

    added file writing to main

--- src/python/main.py
+++ src/python/main.py
@@ -1,5 +1,7 @@
 import os
 import sys    
+import json
+import datetime
 
 # importing modules with their paths
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
@@ -13,8 +15,8 @@ if __name__ == "__main__":
     net_probe = NetworkProbe()
 
     # calling the publics functions for the complete reports
-    sysres = sys_probe.run()
-    netres = net_probe.run()
+    sys_res = sys_probe.run()
+    net_res = net_probe.run()
     
     # printing all the information
 
@@ -22,14 +24,58 @@ if __name__ == "__main__":
     # (like for the debugger bool in the string, a simple print with a concatenation (+) would give an error)
     print("-"*20)
     print("System Report")
-    print(f"User name: {sysres['user']}")
-    print(f"Computer name: {sysres['computer']}")
-    print(f"Debugger active: {sysres['is_debugger_active']}")
+    print(f"User name: {sys_res["user"]}")
+    print(f"Computer name: {sys_res["computer"]}")
+    print(f"Debugger active: {sys_res["is_debugger_active"]}")
+    print(f"System status: {sys_res["status"]}")
 
     print("-"*20)
     print("Network Report")
-    print(f"Number of scanned ports: {netres['scanned_ports_count']}")
-    print(f"Open ports: {netres['open']}")
-    print(f"Closed ports: {netres['closed']}")
-    print(f"Ports returning an error: {netres['error']}")
-    print(f"Network status: {netres['status']}")
+    print(f"Number of scanned ports: {net_res["scanned_ports_count"]}")
+    print(f"Open ports: {net_res["open"]}")
+    print(f"Closed ports: {net_res["closed"]}")
+    print(f"Ports returning an error: {net_res["error"]}")
+    print(f"Network status: {net_res["status"]}")
+
+    # reporting (/output) and logging (/logs)
+
+    # file paths
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    root_path = os.path.dirname(os.path.dirname(current_path))
+    
+    output_file = os.path.join(root_path, "output", "report.json")
+    logs_file = os.path.join(root_path, "logs", "activity.log")
+
+    # current time in iso (wikipedia: "ISO 8601 is an international standard [...] communication of date and time-related data"
+    now = datetime.datetime.now().isoformat()
+
+    # global report dictionnary
+    global_rep = {
+        "time": now,
+        "system_report": sys_res,
+        "network_report": net_res
+    }
+
+    print("-"*20)
+
+    try:
+        # 'w' for write, so we clear the entire file before writing the new report
+        with open(output_file, 'w') as f:
+            json.dump(global_rep, f, indent=4) # using the json library to write in the file f 
+        print(f"Report saved to: " + (output_file))
+    except Exception as e:
+        print("Failed to write report: " + str(e))
+
+    # determining risk level by checking that both reports are all safe
+    risk_level = "Warning" if sys_res["status"] == "Warning" or net_res["status"] == "Warning" else "Safe"
+
+    # written line that will be added to the log file, f strings for the same reason as cited before
+    log_line = f"[{now}] [{risk_level}] Scan complete, ports open: {len(net_res['open'])-1}, debugger: {sys_res['is_debugger_active']}\n"
+
+    try:
+        # 'a' for append, meaning we keep the file as is and just add a new line at the bottom
+        with open(logs_file, 'a') as f:
+            f.write(log_line)
+        print("Log sent to (SIEM simulation): " + str(logs_file))
+    except Exception as e:
+        print("Failed to write logs: " + str(e))
